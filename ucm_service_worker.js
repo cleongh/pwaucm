8218d272ecedc691fd51362cd4b93046f05e043f
Carlos León <cleon@ucm.es>
2020-04-06 23:29:15 +0200

    base

---
+++ ucm_service_worker.js
@@ -0,0 +1,6 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
+
+workbox.precaching.precacheAndRoute([
+  {url: '/index.html', revision: null },
+  {url: '/app.js', revision: null},
+]);
